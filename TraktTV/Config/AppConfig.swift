f275d3f39ad915cbe21d625dba9a46d974636949
dedeexe <dede.exe@gmail.com>
2017-08-19 12:16:37 -0300

    add: Config file

---
+++ TraktTV/Config/AppConfig.swift
@@ -0,0 +1,19 @@
+//
+//  AppConfig.swift
+//  TraktTV
+//
+//  Created by dede.exe on 19/08/17.
+//  Copyright © 2017 dede.exe. All rights reserved.
+//
+
+import Foundation
+
+public final class AppConfig {
+    
+    init() {}
+    
+    public let baseURL  = "https://api.trakt.tv"
+    public let clientId = "cf19f8544fd122d272b78df2e0ef0c651438f847d19f845b6406398c12fb4785"
+    public let secretId = "7086c33bfa6ba882c86f026c58553ca800943e7350a6f091c2dda675908fbbfb"
+    
+}
